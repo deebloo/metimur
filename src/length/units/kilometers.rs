611d4739a9ec59ef4ac45e86adfefb0f8f6071c9
deebloo <dannybluedesign@gmail.com>
2023-11-07 20:26:01 -0500

    as_kilometers

--- src/length/units/kilometers.rs
+++ src/length/units/kilometers.rs
@@ -3,10 +3,10 @@ use crate::Length;
 impl Length {
     pub fn as_kilometers(&self) -> Self {
         match self {
-            Self::In(val) => Self::KM(val * 25.4),
-            Self::Ft(val) => Self::KM(val * 304.8),
-            Self::Yd(val) => Self::KM(val * 914.4),
-            Self::Mi(val) => Self::KM(val * 1.609e+6),
+            Self::In(val) => Self::KM(val / 39370.),
+            Self::Ft(val) => Self::KM(val / 3281.),
+            Self::Yd(val) => Self::KM(val / 1094.),
+            Self::Mi(val) => Self::KM(val * 1.609),
             Self::MM(val) => Self::KM(val / 1e+6),
             Self::CM(val) => Self::KM(val / 100000.),
             Self::M(val) => Self::KM(val / 1000.),
@@ -16,9 +16,11 @@ impl Length {
 }
 
 #[test]
-fn should_convert_to_milimeters() {
-    assert_eq!(Length::MM(25.4), Length::In(1.).as_milimeters());
-    assert_eq!(Length::MM(304.8), Length::Ft(1.).as_milimeters());
-    assert_eq!(Length::MM(914.4), Length::Yd(1.).as_milimeters());
-    assert_eq!(Length::MM(1.609e+6), Length::Mi(1.).as_milimeters());
+fn should_convert_to_kilometers() {
+    assert_eq!(Length::KM(1.), Length::In(39370.).as_kilometers());
+    assert_eq!(Length::KM(1.), Length::Ft(3281.).as_kilometers());
+    assert_eq!(Length::KM(1.609), Length::Mi(1.).as_kilometers());
+    assert_eq!(Length::KM(1.), Length::MM(1e+6).as_kilometers());
+    assert_eq!(Length::KM(1.), Length::CM(100000.).as_kilometers());
+    assert_eq!(Length::KM(1.), Length::M(1000.).as_kilometers());
 }
